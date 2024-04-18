1703522a08e9ef3bc68d33ba67b8adb750035aa4
Sky <skyblue3742@gmail.com>
2024-04-19 01:35:50 +0800

    add drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import type { Config } from "drizzle-kit";
+
+export default {
+	schema: "./src/data/db/schemas.ts",
+	out: "./drizzle",
+	driver: "pg", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+	dbCredentials: {
+		host: process.env.POSTGRES_HOST!,
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DATABASE!,
+		ssl: true
+	}
+} satisfies Config;
